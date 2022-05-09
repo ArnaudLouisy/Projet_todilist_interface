facd0154b7134b930f8f78ce5e81207eee397e9e
TCHIBOZO_Fr <totofrancois12@gmail.com>
2022-05-09 17:14:50 +0200

    fead : ajout de fichier

---
+++ src/main/java/appli/tache/Acceuil.java
@@ -0,0 +1,18 @@
+package appli.tache;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Acceuil {
+
+    @FXML
+    private Button Profile;
+
+    @FXML
+    void onClikProfile(ActionEvent event) {
+
+    }
+
+}
+
